45efd21ef6dcb3fe8fc4b410e1cce5dc509868b1
Maisha Umama <umamamaisha@gmail.com>
2020-09-21 21:58:36 +0600

    upto middleware function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req,res) => {
     res.send (`
@@ -15,10 +18,13 @@ app.get('/', (req,res) => {
     `);
 });
 
+
+
 app.post('/', (req,res) =>{
+  console.log(req.body);
     res.send('account created!!!!'); 
 });
 
 app.listen(3000, () => {
     console.log('listening');
-}); 
+});  
