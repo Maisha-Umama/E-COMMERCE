b460ef1d44a213e71d6ffeb8bc77fe2d55d6af26
Maisha Umama <umamamaisha@gmail.com>
2020-09-23 22:38:19 +0600

    users reposiotry methods part1

--- repositores/user.js
+++ repositores/user.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository{
     constructor(filename){
@@ -15,18 +16,50 @@ class UsersRepository{
     }
 
     async getAll() {
-        const contents = await fs.promises.readFile(this.filename, {
+        return  JSON.parse(await fs.promises.readFile(this.filename, {
             encoding: 'utf8'
-        });
-
-        console.log(contents);
+        })
+        );
     }
-}
+
+    async create(attrs){
+        attrs.id = this.randomId();
+        const records = await this.getAll();
+        records.push(attrs);
+
+        await this.writeAll(records);
+    }
+
+    async writeAll(records){
+        await fs.promises.writeFile(
+            this.filename, 
+            JSON.stringify(records, null, 2)
+            );
+    }
+
+    randomId(){
+        return crypto.randomBytes(4).toString('hex');
+    }
+
+    async getOne(id){
+        const records = await this.getAll();
+        return records.find(record => record.id == id);
+
+    }
+
+    async delete(id){
+         const records = await this.getAll();
+         const filterRecords = records.filter(record => record.id !== id);
+         await this.writeAll(filterRecords);
+    }
+
+
+} 
 
 const test = async() => {
      const repo = new UsersRepository('users.json');
 
-     await repo.getAll();
+     await repo.delete("fed731ff");
 
  
 };
