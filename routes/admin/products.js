ea87973f2259d7403b87effb9cd24bf30b6f1c55
Maisha Umama <umamamaisha@gmail.com>
2020-10-04 20:25:52 +0600

    product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
+const { validationResult } = require ('express-validator'); 
+const productsRepo = require('../../repositores/products');
+const productsNewTemplate = require('../../views/admin/products/new')
+const { requirePrice, requireTitle }= require('./validator');
 const router = express.Router();
 
 router.get('/admin/products', (req,res) => {});
 
-router.get('/admin/products/new', (req,res) => {});
+router.get('/admin/products/new', (req,res) => {
+    res.send(productsNewTemplate({}));
+});
 
-module.exports = router;
+router.post('/admin/products/new' , [requireTitle,requirePrice],(req,res)=>{
+    const errors = validationResult(req);
+    console.log(errors);
+
+    res.send('submitted');
+})
+
+module.exports = router; 
