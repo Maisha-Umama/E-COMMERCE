f428a1b41150f76113f55791e0bc3502046f73ce
Maisha Umama <umamamaisha@gmail.com>
2020-10-05 22:13:27 +0600

    middleware bug

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { validationResult } = require ('express-validator'); 
+const multer = require('multer');
+
 const productsRepo = require('../../repositores/products');
 const productsNewTemplate = require('../../views/admin/products/new')
-const { requirePrice, requireTitle }= require('./validator');
+const { requireTitle,requirePrice  }= require('./validator');
+
 const router = express.Router();
+const upload =multer({ storage: multer.memoryStorage() }); 
 
 router.get('/admin/products', (req,res) => {});
 
@@ -11,11 +15,20 @@ router.get('/admin/products/new', (req,res) => {
     res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new' , [requireTitle,requirePrice],(req,res)=>{
-    const errors = validationResult(req);
-    console.log(errors);
+router.post('/admin/products/new' ,
+  upload.single('image'),
+ [requireTitle,requirePrice],
+async(req,res)=>{
+   const errors = validationResult(req);
+
+   if(!errors.isEmpty()){
+       return res.send(productsNewTemplate({ errors }));
+   }
+   const image =req.file.buffer.toString('base64');
+   const { title,price } = req.body;
+   await productsRepo.create({ title, price, image });
 
     res.send('submitted');
 })
 
-module.exports = router; 
+module.exports = router;  
