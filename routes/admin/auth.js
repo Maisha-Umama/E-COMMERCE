29adeb23b59330ec460bfeb590538ffa832b6fac
Maisha Umama <umamamaisha@gmail.com>
2020-10-16 21:05:58 +0600

    products listing

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check,validationResult } = require ('express-validator');
 
+const { handleErrors } = require('./middleware');
 const usersRepo = require('../../repositores/user');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin'); 
@@ -19,14 +19,9 @@ router.get('/signup', (req,res) => {
 router.post(
   '/signup',
   [ requireEmail, requirePassword,requirePasswordConfirmation  ] 
- ,async  (req,res) =>{
-   const errors = validationResult(req);
-  
-   if(!errors.isEmpty()){
-     return res.send(signupTemplate({ req, errors }));
-  }
-  
-  const { email, password, passwordConfirmation } =req.body;
+ ,handleErrors(signupTemplate),
+ async  (req,res) =>{
+  const { email, password } =req.body;
   const user = await usersRepo.create( { email, password });
 
   req.session.userId = user.id;
@@ -45,12 +40,9 @@ router.get('/signin', (req,res) => {
 });
 
 router.post('/signin',[requireEmailExists,requireValidPasswordForUser ], 
- async(req,res) =>{
-   const errors = validationResult(req);
-   
-   if(!errors.isEmpty()) {
-     return res.send(signinTemplate({ errors }));
-   }
+ handleErrors(signinTemplate),
+async(req,res) =>{
+
 
   const { email } = req.body;
   
